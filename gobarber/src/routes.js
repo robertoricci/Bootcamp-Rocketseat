6eac3c10f5cf3e6e0001508e84b6542cd2eb18fc
robertoricci <roberto.rricci@gmail.com>
2019-09-13 17:17:01 -0300

    Modulo_criandolodermodels

--- gobarber/src/routes.js
+++ gobarber/src/routes.js
@@ -1,9 +1,18 @@
 import { Router } from 'express';
 
+import User from './app/models/User';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'ola new' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Roberto.ricci',
+    email: 'roberto@teste.com.br',
+    password_hash:'123456',
+  })
+
+  return res.json(user);
 });
 
 export default routes;
+
